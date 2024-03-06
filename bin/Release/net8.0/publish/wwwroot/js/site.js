f29d33b00024f11b7a7977bf20a20a4092985e9f
Mitravarun Pandey <mitravarunpandeybaba@gmail.com>
2024-03-06 15:08:50 +0530

    First Uploade

---
+++ bin/Release/net8.0/publish/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿
+    function GoToHome() {
+        debugger
+    var term = document.getElementById("searchTerm").value;
+    var currentPage = 1; 
+
+    // Make the AJAX call
+    $.ajax({
+        type: "GET",
+    url: "/Home/Index",
+    data: {term: term, currentPage: currentPage },
+    success: function (data) {debugger
+        toastr.success('Welcome to Move Adda', 'Success');
+            },
+    error: function (xhr, status, error) {
+        // Handle the error, such as displaying an error message
+        console.error(xhr.responseText);
+            }
+        });
+    }
+
